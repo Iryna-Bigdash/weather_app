13042353954bb58094972b4e067aedc64ff69278
Iryna B <imbigdash@gmail.com>
2022-11-18 16:26:02 +0000

    added conv.from cels to fahr

--- crs/st.js
+++ crs/st.js
@@ -1,5 +1,3 @@
-// Link 
-// https://codesandbox.io/s/elastic-leaf-85qwnv?file=/index.html
 
 let now = new Date();
 let curretDate = document.querySelector("#current-date")
@@ -16,7 +14,7 @@ let iconElement = document.querySelector("#icon");
 
 
 function displayWeatherCondition(response){
-    console.log(response.data);
+    // console.log(response.data);
     document.querySelector("#city").innerHTML = response.data.name;
     document.querySelector("#temperature").innerHTML = Math.round(response.data.main.temp);
     document.querySelector("#humidity").innerHTML = response.data.main.humidity;
@@ -27,6 +25,8 @@ function displayWeatherCondition(response){
     document.querySelector("#current-city").innerHTML = response.data.name;
     iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
     iconElement.setAttribute("alt", response.data.weather[0].description);
+
+    celsiusTemperature = response.data.main.temp;
 }
 
 function searchCity(city) {
@@ -55,20 +55,47 @@ function getCurrentLocation(event) {
 }
 
 
+// function convertToFahrenheit(event) {
+//     event.preventDefault();
+//     let temperatureElement = document.querySelector("#temperature");
+//     let temperature = temperatureElement.innerHTML;
+//     temperature = Number(temperature);
+//     temperatureElement.innerHTML = Math.round((temperature * 9) / 5 + 32);
+// }
+
+// let fahrenheitLink = document.querySelector("#fahrenheit-link");
+// fahrenheitLink.addEventListener("click", convertToFahrenheit);
+
+function displayFahrenheitTemperature(event) {
+    event.preventDefault();
+    let temperatureElement = document.querySelector("#temperature");  
+     
+    celsiusLink.classList.remove("active");
+    fahrenheitLink.classList.add("active");
+   
+    
+    let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+    temperatureElement.innerHTML = Math.round(fahrenheitTemperature); 
+}
+function displayCelsiusTemperature(event) {
+    event.preventDefault();
+   
+    celsiusLink.classList.add("active");
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(celsiusTemperature); 
+}
+
+let celsiusTemperature = null;
+
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSubmit);
 
-
-function convertToFahrenheit(event) {
-    event.preventDefault();
-    let temperatureElement = document.querySelector("#temperature");
-    let temperature = temperatureElement.innerHTML;
-    temperature = Number(temperature);
-    temperatureElement.innerHTML = Math.round((temperature * 9) / 5 + 32);
-}
-
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", convertToFahrenheit);
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
 
 let currentLocationButton = document.querySelector("#current-location-button");
 currentLocationButton.addEventListener("click", getCurrentLocation);
@@ -79,7 +106,6 @@ searchCity("New York");
 
 
 
-// // Home Work
 
 
 
@@ -91,9 +117,6 @@ searchCity("New York");
 
 
 
-
-//   let h2 = document.querySelector("#city-name");
-//   h2.innerHTML = city;
 
   
 
@@ -105,42 +128,3 @@ searchCity("New York");
 
 
 
-// // // Current temperature
-// let apiKey = "5da7b2dc058f07286fea39c4cee516a3";
-// // // let city = "Riga"
-// let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric`;
-
-
-
-// // function showTemperature(response) {
-// //     // alert(`The current temperature is ${response.data.name}`);
-// //     console.log(response.data);
-
-// //     let temperature = Math.round(response.data.main.temp);
-// //     let temperatureElement = document.querySelector("#temperature");
-// //     let description = document.querySelector("#temperature-description");
-// //     // let minTemperature = document.querySelector("#min-temp");
-// //     temperatureElement.innerHTML = `${temperature}`;
-// //     description.innerHTML = response.data.weather[0].description;
-// //     // minTemperature.innerHTML = response.data.weather[0].temp_min;
-// //   }
-
-// //   let h2 = document.querySelector("#city-name");
-// //   h2.innerHTML = city;
-
-
-// let url = "https://jsonplaceholder.typicode.com/users/1";
-// //   axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemperature);
-
-// // // Log current temperature depend the GEO LOCATION
-
-// // function showPosition(position) {
-// //     let h6 = document.querySelector("h6")
-// //     h6.innerHTML =`Your Latitued is ${position.coords.latitude} and your longitude is ${position.coords.longitude}`;
-
-// //   }
-// //   function getCurrentPosition() {
-// //     navigator.geolocation.getCurrentPosition(showPosition);
-// // }
-// //   let button = document.querySelector("#current-location-button");
-// //   button.addEventListener("click", getCurrentPosition);
